f09ef37307847fbdedda40751e82ea76abf64265
LIGHT1213 <35650324+LIGHT1213@users.noreply.github.com>
2020-05-27 10:53:18 +0800

    Update wifi_configuration.h

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -10,8 +10,8 @@
 #ifndef __WIFI_CONFIGURATION__
 #define __WIFI_CONFIGURATION__
 
-#define WIFI_SSID "DAP"
-#define WIFI_PASS "12345678"
+#define WIFI_SSID "DeBug406"
+#define WIFI_PASS "debug406"
 
 #define PORT 3240
 
